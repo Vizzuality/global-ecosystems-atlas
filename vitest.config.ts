e463df1bfb2c90e7075a384ce84aa9bc6790f9d4
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-08-13 10:53:03 +0200

    Vitest

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import path from "path";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
