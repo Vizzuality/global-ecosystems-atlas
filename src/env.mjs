19482f2037fb8129452a3406a4a4ab80be439e0b
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-08-20 10:56:09 +0200

    Contact: states

--- src/env.mjs
+++ src/env.mjs
@@ -7,15 +7,15 @@ export const env = createEnv({
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: z.string(),
     NEXT_PUBLIC_API_URL: z.preprocess(
       // This makes Vercel deployments not fail if you don't set NEXT_PUBLIC_API_URL
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesnt include `https` so it cant be validated as a URL
-      process.env.VERCEL_URL ? z.string() : z.string().url().min(1),
+      (str) => process.env.NEXT_PUBLIC_VERCEL_URL ?? str,
+      // NEXT_PUBLIC_VERCEL_URL doesnt include `https` so it cant be validated as a URL
+      process.env.NEXT_PUBLIC_VERCEL_URL ? z.string() : z.string().url().min(1),
     ),
   },
   runtimeEnv: {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    NEXT_PUBLIC_API_URL: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
       : process.env.NEXT_PUBLIC_API_URL,
   },
 });
