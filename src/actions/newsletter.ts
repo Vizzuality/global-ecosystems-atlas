6b394605a4b779a5153c0398402c9240e7c1caf3
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-08-21 21:26:55 +0200

    Newsletter form

---
+++ src/actions/newsletter.ts
@@ -0,0 +1,42 @@
+"use server";
+
+// import nodemailer from "nodemailer";
+
+import { z } from "zod";
+
+import { newsletterFormSchema } from "@/lib/newsletter";
+
+export async function newsletterAction(data: z.infer<typeof newsletterFormSchema>) {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve({ error: false, success: true, errors: [], response: data });
+    }, 1000),
+  );
+
+  // const transporter: nodemailer.Transporter = nodemailer.createTransport({
+  //   host: process.env.NEXT_PUBLIC_EMAIL_HOST,
+  //   port: 587,
+  //   secure: false,
+  //   auth: {
+  //     user: process.env.NEXT_PUBLIC_EMAIL_USER,
+  //     pass: process.env.NEXT_PUBLIC_EMAIL_PASS,
+  //   },
+  // });
+
+  // const mailOptions: nodemailer.SendMailOptions = {
+  //   from: request.email, // sender address
+  //   to: process.env.NEXT_PUBLIC_EMAIL_SEND_TO, // list of receivers
+  //   subject: `Contact`, // Subject line
+  //   text: request.message, // plain text body
+  //   html: `<div>${request.message}</div>`, // html body
+  // };
+
+  // return await transporter
+  //   .sendMail(mailOptions)
+  //   .then((response: nodemailer.SentMessageInfo) => {
+  //     return { error: false, success: true, errors: [], response };
+  //   })
+  //   .catch((error: nodemailer.SentMessageInfo) => {
+  //     return { error: true, success: false, errors: [error] };
+  //   });
+}
--- src/containers/newsletter/index.tsx
+++ src/containers/newsletter/index.tsx
@@ -1,11 +1,14 @@
+import { NewsletterForm } from "@/containers/newsletter/form";
 import { NewsletterStayTuned } from "@/containers/newsletter/stay-tuned";
 
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 export const Newsletter = () => {
   return (
     <>
       <NewsletterStayTuned />
+
       <div className="grid grid-cols-12 gap-6 py-20">
         <div className="col-span-12 md:col-span-8 md:col-start-3">
           <div className="text-center">
@@ -13,9 +16,17 @@ export const Newsletter = () => {
             <p className="mt-4 text-xl">
               Be the first to know about the full launch of the Global Ecosystems Atlas.
             </p>
-            <Button size="lg" className="mt-12">
-              Sign up for updates
-            </Button>
+
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size="lg" className="mt-12">
+                  Sign up for updates
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <NewsletterForm />
+              </DialogContent>
+            </Dialog>
           </div>
         </div>
       </div>
