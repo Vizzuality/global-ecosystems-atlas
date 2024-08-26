596870fcc2f4df3aea2c6dd2175a0ecf18c4a693
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-08-26 16:41:17 +0200

    Stay tuned: style fix

--- src/containers/newsletter/index.tsx
+++ src/containers/newsletter/index.tsx
@@ -10,28 +10,30 @@ export const Newsletter = () => {
     <>
       <NewsletterStayTuned />
 
-      <Grid className="py-20">
-        <div className="col-span-12 sm:col-span-8 sm:col-start-3">
-          <div className="text-center">
-            <h3 className="text-lg font-bold uppercase">Sign up</h3>
-            <p className="mt-4 text-xl">
-              Be the first to know about the full launch of the Global Ecosystems Atlas.
-            </p>
+      <div className="container">
+        <Grid className="py-20">
+          <div className="col-span-12 sm:col-span-8 sm:col-start-3">
+            <div className="text-center">
+              <h3 className="text-lg font-bold uppercase">Sign up</h3>
+              <p className="mt-4 text-xl">
+                Be the first to know about the full launch of the Global Ecosystems Atlas.
+              </p>
 
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button size="lg" className="mt-12">
-                  Sign up for updates
-                </Button>
-              </DialogTrigger>
-              <DialogContent aria-describedby="newsletter-form">
-                <DialogDescription />
-                <NewsletterForm />
-              </DialogContent>
-            </Dialog>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button size="lg" className="mt-12">
+                    Sign up for updates
+                  </Button>
+                </DialogTrigger>
+                <DialogContent aria-describedby="newsletter-form">
+                  <DialogDescription />
+                  <NewsletterForm />
+                </DialogContent>
+              </Dialog>
+            </div>
           </div>
-        </div>
-      </Grid>
+        </Grid>
+      </div>
     </>
   );
 };
