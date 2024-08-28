2bcee6e852bc12f4276da21deaad26b512c63dcb
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-08-28 17:51:56 +0200

    Data: methodology and how to use starting point

--- src/app/(app)/data/how-to-use-the-data/page.tsx
+++ src/app/(app)/data/how-to-use-the-data/page.tsx
@@ -1,5 +1,8 @@
 import { Metadata } from "next";
 
+import { Grid } from "@/components/ui/grid";
+import { Section } from "@/components/ui/section";
+
 export const metadata: Metadata = {
   title: "How to use the data | Global Ecosystems Atlas",
   description: "How to use the data Global Ecosystems Atlas description",
@@ -7,8 +10,28 @@ export const metadata: Metadata = {
 
 export default async function HowToUseTheData() {
   return (
-    <div>
-      <h1>How to use the data</h1>
-    </div>
+    <Section>
+      <div className="container">
+        <Grid>
+          <div className="col-span-12 lg:col-span-3 lg:col-start-2">
+            <h3 className="text-3xl font-semibold">Data product</h3>
+          </div>
+
+          <div className="col-span-12 lg:col-span-7 lg:col-start-5">
+            <p className="text-lg">
+              The Global Ecosystems Atlas Basemap (gea_basemap) is a data product designed to
+              support environmental initiatives by providing a{" "}
+              <strong>consistent spatial dataset</strong> representing ecosystems worldwide. The
+              development of the basemap involved the compilation and harmonization of existing,
+              publicly available assets that are known to represent ecosystems and that meet
+              stringent data quality standards. Key processes in the development of the basemap
+              include data compilation, quality assurance, assigning map classes to their
+              corresponding IUCN Global Ecosystem Typology class, and integration into a data
+              product that can support a variety of use cases. 
+            </p>
+          </div>
+        </Grid>
+      </div>
+    </Section>
   );
 }
--- src/app/(app)/data/methodology/page.tsx
+++ src/app/(app)/data/methodology/page.tsx
@@ -1,5 +1,8 @@
 import { Metadata } from "next";
 
+import { Grid } from "@/components/ui/grid";
+import { Section } from "@/components/ui/section";
+
 export const metadata: Metadata = {
   title: "Methodology | Global Ecosystems Atlas",
   description: "Methodology Global Ecosystems Atlas description",
@@ -7,8 +10,28 @@ export const metadata: Metadata = {
 
 export default async function Methodology() {
   return (
-    <div>
-      <h1>Methodology</h1>
-    </div>
+    <Section>
+      <div className="container">
+        <Grid>
+          <div className="col-span-12 lg:col-span-3 lg:col-start-2">
+            <h3 className="text-3xl font-semibold">Data Compilation</h3>
+          </div>
+
+          <div className="col-span-12 lg:col-span-7 lg:col-start-5">
+            <p className="text-lg">
+              The gea_basemap was created by searching for and compiling existing spatial data
+              products that were intended to represent ecosystems Searches focused on publicly
+              available data repositories, the scientific literature, and through a coordinated
+              program of targeted outreach focused primarily on national ecosystem mapping
+              organisations. Each dataset was subjected to a <strong>rigorous evaluation</strong>{" "}
+              and quality assessment protocol that included formal assessment of license conditions,
+              class definitions, validations and accuracy assessment, data freshness, and spatial
+              resolution. The evaluation protocol ensured the inclusion of only the highest-quality
+              data into the gea_basemap. 
+            </p>
+          </div>
+        </Grid>
+      </div>
+    </Section>
   );
 }
