c87a6777433c5a19a208ec7c914ce77cced5ebbb
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-08-29 16:23:05 +0200

    Data: sources catalogue table

--- src/containers/data/sources-catalogue/table/columns.tsx
+++ src/containers/data/sources-catalogue/table/columns.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { FiArrowUpRight } from "react-icons/fi";
 
 import { Dataset } from "@/types/dataset";
 
@@ -22,8 +23,14 @@ export const columns: ColumnDef<Dataset>[] = [
     header: "Link",
     enableSorting: false,
     cell: (cell) => (
-      <a href={cell.row.getValue("URL")} target="_blank" rel="noreferrer">
-        Dataset
+      <a
+        href={cell.row.getValue("URL")}
+        target="_blank"
+        rel="noreferrer"
+        className="flex items-center gap-1"
+      >
+        <span>Dataset</span>
+        <FiArrowUpRight className="h-6 w-6" />
       </a>
     ),
   },
