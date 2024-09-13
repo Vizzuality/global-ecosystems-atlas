5d98d5bb6f2b8b569ef1d24e2b4254d048e1687b
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-09-13 11:20:22 +0200

    Search: combobox

--- src/app/(atlas)/atlas/parsers.ts
+++ src/app/(atlas)/atlas/parsers.ts
@@ -1,5 +1,7 @@
 import { parseAsArrayOf, parseAsFloat, parseAsStringLiteral } from "nuqs";
 
+import { COUNTRIES } from "@/lib/countries";
+
 import { BASEMAPS } from "@/containers/atlas/map/basemaps";
 
 export const bboxParser = parseAsArrayOf(parseAsFloat).withDefault([-227.18, -53.92, 88.88, 71.99]);
@@ -7,3 +9,5 @@ export const bboxParser = parseAsArrayOf(parseAsFloat).withDefault([-227.18, -53
 export const basemapParser = parseAsStringLiteral(BASEMAPS.map((b) => b.value)).withDefault(
   "light",
 );
+
+export const countryParser = parseAsStringLiteral(COUNTRIES.map((c) => c.iso3));
--- src/app/(atlas)/atlas/store.ts
+++ src/app/(atlas)/atlas/store.ts
@@ -3,7 +3,7 @@
 import { atom } from "jotai";
 import { createSerializer, useQueryState } from "nuqs";
 
-import { basemapParser, bboxParser } from "@/app/(atlas)/atlas/parsers";
+import { basemapParser, bboxParser, countryParser } from "@/app/(atlas)/atlas/parsers";
 
 export const navOpenAtom = atom(false);
 export const sidebarOpenAtom = atom(true);
@@ -16,16 +16,24 @@ export const useSyncBasemap = () => {
   return useQueryState("basemap", basemapParser);
 };
 
+export const useSyncCountry = () => {
+  return useQueryState("country", countryParser);
+};
+
 const serialize = createSerializer({
   bbox: bboxParser,
   basemap: basemapParser,
+  country: countryParser,
 });
 
 export const useSyncSearchParams = () => {
   const [bbox] = useSyncBbox();
   const [basemap] = useSyncBasemap();
+  const [country] = useSyncCountry();
+
   return serialize({
     bbox,
     basemap,
+    country,
   });
 };
