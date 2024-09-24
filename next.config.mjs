28f01ea690d38092ebd0e0d1a3322ba9d8635a26
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-09-24 08:52:29 +0200

    Home: video

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 // Import env here to validate during build. Using jiti we can import .ts files :)
 import("./src/env.mjs");
 
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   images: {
     unoptimized: true,
