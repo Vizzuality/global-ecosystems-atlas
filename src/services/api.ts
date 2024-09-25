1bccdd7bc11b533d60fb898e12338b475d031543
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-09-25 10:00:41 +0200

    Ecosysytem list: detail static generation

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,12 @@
 import Axios, { AxiosError, AxiosRequestConfig } from "axios";
 // import qs from "query-string";
 
-import { env } from "@/env.mjs";
+// import { env } from "@/env.mjs";
 
-const BASE_URL = env.NEXT_PUBLIC_API_URL;
+// const BASE_URL = env.NEXT_PUBLIC_API_URL;
 
 export const AXIOS_INSTANCE = Axios.create({
-  baseURL: BASE_URL,
+  baseURL: "https://global-ecosystem-atlas.staging.dev-vizzuality.com/api",
 });
 
 export const API = <T>(config: AxiosRequestConfig, options?: AxiosRequestConfig): Promise<T> => {
