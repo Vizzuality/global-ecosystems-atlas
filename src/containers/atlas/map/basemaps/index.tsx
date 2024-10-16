0422e373eb5b251055f945d987cc30c57161bf91
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-10-16 16:57:44 +0200

    Map: default bbox, mapStyles

--- src/containers/atlas/map/basemaps/index.tsx
+++ src/containers/atlas/map/basemaps/index.tsx
@@ -7,19 +7,19 @@ export const BASEMAPS = [
     label: "Default",
     value: "light" as const,
     preview: `/atlas/basemaps/light.png`,
-    mapStyle: "mapbox://styles/mapbox/light-v10",
+    mapStyle: "mapbox://styles/vizzgea/clzd26iv600a001qw50u1ga2i",
   },
   {
     label: "Terrain",
     value: "terrain" as const,
     preview: `/atlas/basemaps/terrain.png`,
-    mapStyle: "mapbox://styles/mapbox/outdoors-v12",
+    mapStyle: "mapbox://styles/vizzgea/cm2bwqmnq00rq01pgbx9e5qf1",
   },
   {
     label: "Satellite",
     value: "satellite" as const,
     preview: `/atlas/basemaps/satellite.png`,
-    mapStyle: "mapbox://styles/mapbox/satellite-v9",
+    mapStyle: "mapbox://styles/vizzgea/cm2bwsl0900rs01pgb4dz6kcy",
   },
 ];
 
