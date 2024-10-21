70cbdf2b502d96bdfe2c59050145c1cd146ee1ce
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2024-10-21 09:26:39 +0200

    Favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -7,6 +7,13 @@ import { mediaStyles } from "@/containers/media";
 function RootHead() {
   return (
     <head>
+      <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+      <link rel="manifest" href="/site.webmanifest" />
+
       <style key="fresnel-css" dangerouslySetInnerHTML={{ __html: mediaStyles }} type="text/css" />
     </head>
   );
